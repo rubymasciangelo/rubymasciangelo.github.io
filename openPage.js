b58051e0af32868f2fa2f607c52f2242cb349606
rubymasciangelo <rubymasciangelo@gmail.com>
2020-11-17 10:27:58 -0800

    tabs added

---
+++ openPage.js
@@ -0,0 +1,14 @@
+function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
