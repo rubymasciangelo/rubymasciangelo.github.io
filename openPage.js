7b72737558865d25c49e45121c1f7867bf4ad909
rubymasciangelo <rubymasciangelo@gmail.com>
2021-02-23 11:25:04 -0800

    tab differentiation omg

--- openPage.js
+++ openPage.js
@@ -1,13 +1,22 @@
-function openPage(pageName, elmnt) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
+function openPage(evt, pageName) {
+    // Declare all variables
+    var i, tabcontent, tablink;
+
+    // Hide all elements with class="tabcontent" by default
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
+
+    // Get all elements with class="tablink" and remove the class "active"
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+    }
   
-    // Show the specific tab content
+    // Show the specific tab content, and add an "active" class to the button that opened the tab
     document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
   
   // Get the element with id="defaultOpen" and click on it
